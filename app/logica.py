d8574607a5e887afde9ef34fd865001f9cdb6c34
Julio <juliomt0401@gmail.com>
2026-09-05 09:38:24 -0500

    calcular_factoring

--- app/logica.py
+++ app/logica.py
@@ -1,7 +1,10 @@
 import requests
+import os
 from decimal import Decimal, ROUND_HALF_UP
+from datetime import date, timedelta
 from app.bd import BD
 
+RUC_FACTOR = os.getenv("RUC_FACTOR")
 
 def calcular_TEM_desde_TEA(TEA: Decimal) -> Decimal:
     TEM = (Decimal("1") + TEA) ** (Decimal("1") / Decimal("12")) - Decimal("1")
@@ -16,6 +19,59 @@ def calcular_TED_desde_TEA(TEA: Decimal) -> Decimal:
 def calcular_TED_desde_TEM(TEM: Decimal) -> Decimal:
     TED = (Decimal("1") + TEM) ** (Decimal("1") / Decimal("30")) - Decimal("1")
     return TED.quantize(Decimal("0.000001"), rounding=ROUND_HALF_UP)
+
+
+def obtener_factor_vigente_IGV() -> Decimal:
+    sql = """
+        SELECT Valor1 FROM sys_parametros WHERE RUC_Factor = %s AND idParametro = '0002' AND Estado = 'V'
+    """
+    db = BD()
+    recordset = db.ejecutar_SQL(sql, (RUC_FACTOR,))
+    if not recordset:
+        return Decimal("0.00")  # Valor por defecto si no se encuentra en la base de datos
+    igv_vigente = Decimal(recordset[0]["Valor1"])
+    return igv_vigente
+
+
+def calcular_factoring(tea: Decimal, factor_adelanto: Decimal, importe: Decimal, fecha_pago: date) -> dict:
+
+    # Plazo de financiamiento
+    fecha_inicio = date.today() + timedelta(days=1)
+    plazo = (fecha_pago - fecha_inicio).days + 1
+
+    # Tasas equivalentes
+    tem = calcular_TEM_desde_TEA(tea)
+    ted = calcular_TED_desde_TEA(tea)
+
+    # Adelanto
+    importe_adelanto = round( round(importe * factor_adelanto, 3), 2)
+
+    # Interés anticipado
+    uno = Decimal("1")
+    interes = round( round( (uno - (uno / ((uno + ted) ** plazo))) * importe_adelanto, 3), 2)
+
+    # IGV
+    igv = round( round(interes * obtener_factor_vigente_IGV(), 3), 2)
+
+    # Importe a desembolsar
+    importe_desembolsar = (importe_adelanto - interes - igv)
+
+    # Importe remanente
+    importe_remanente = (importe - importe_adelanto)
+
+    return {
+        "TEA": tea,
+        "TEM": tem,
+        "TED": ted,
+        "importe": importe,
+        "FactorAdelanto": factor_adelanto,
+        "ImporteAdelanto": importe_adelanto,
+        "Plazo": plazo,
+        "Interes": interes,
+        "IGV": igv,
+        "ImporteDesembolsar": importe_desembolsar,
+        "ImporteRemanente": importe_remanente
+    }
 
 
 def consultar_ruc_api(RUC: str) -> str:
@@ -43,7 +99,7 @@ def consultar_ruc_api(RUC: str) -> str:
         return ""
 
 
-def buscar_descripcion_tabla(tabla: str, elemento: str) -> str | None:
+def buscar_descripcion_tabla(tabla: str, elemento: str) -> str:
     sql = """
         SELECT Descripcion
         FROM sys_tablas
--- app/models/adquirente.py
+++ app/models/adquirente.py
@@ -4,34 +4,53 @@ from app.bd import BD
 from app.logica import calcular_TEM_desde_TEA, consultar_ruc_api
 
 
-def _calcular_tea_adquirente_por_ranking_top(RUC: str) -> Decimal:
+def _calcular_TEA_adquirente_por_ranking_top(RUC: str) -> Decimal:
 
     sql = """
-        SELECT Ranking
-        FROM Top10K
-        WHERE RUC = %s
+        SELECT Ranking FROM Top10K WHERE RUC = %s
     """
     db = BD()
     recordset = db.ejecutar_SQL(sql, (RUC,))
     if not recordset:
         return Decimal("0.3000")
     ranking = recordset[0]["Ranking"]
+    bloque = ((ranking - 1001) // 200) + 1
 
     # Top 1 al 1000
-    if ranking <= 1000:
-        return Decimal("0.1200")
+    if ranking <= 1000: return Decimal("0.1200")
 
     # Top 1001 al 3000
-    if ranking <= 3000:
-        bloque = ((ranking - 1001) // 200) + 1
-        return (Decimal("0.1200") + Decimal(bloque) * Decimal("0.0020"))
+    if ranking <= 3000: return (Decimal("0.1200") + Decimal(bloque) * Decimal("0.0020"))
 
     # Top 3001 al 5000
-    if ranking <= 5000:
-        return Decimal("0.1600")
+    if ranking <= 5000: return (Decimal("0.1400") + Decimal(bloque) * Decimal("0.0020"))
 
     # Fuera de política
     return Decimal("0.3000")
+
+
+def _calcular_FactorAdelanto_adquirente_por_ranking_top(RUC: str) -> Decimal:
+
+    sql = """
+        SELECT Ranking FROM Top10K WHERE RUC = %s
+    """
+    db = BD()
+    recordset = db.ejecutar_SQL(sql, (RUC,))
+    if not recordset:
+        return Decimal("0.9000")
+    ranking = recordset[0]["Ranking"]
+
+    # Top 1 al 1000
+    if ranking <= 1000: return Decimal("0.9900")
+
+    # Top 1001 al 3000
+    if ranking <= 3000: return Decimal("0.9800")
+
+    # Top 3001 al 5000
+    if ranking <= 5000: return Decimal("0.9700")
+
+    # Fuera de política
+    return Decimal("0.9000")
 
 
 class Adquirente:
@@ -46,9 +65,10 @@ class Adquirente:
         Telefono: str | None = None,
         TipoContribuyente: str | None = None,
         UBIGEO: str | None = None,
-        LineaAsignada: Decimal = Decimal("0"),
-        TEA: Decimal = Decimal("0"),
-        TEM: Decimal = Decimal("0"),
+        LineaAsignada: Decimal = Decimal("0.00"),
+        TEA: Decimal = Decimal("0.0000"),
+        TEM: Decimal = Decimal("0.0000"),
+        FactorAdelanto: Decimal = Decimal("0.0000"),
         Estado: str | None = None
     ):
         self.idAdquirente = idAdquirente
@@ -62,6 +82,7 @@ class Adquirente:
         self.LineaAsignada = LineaAsignada
         self.TEA = TEA
         self.TEM = TEM
+        self.FactorAdelanto = FactorAdelanto
         self.Estado = Estado
 
 
@@ -72,7 +93,7 @@ class Adquirente:
         #Si no existe, idAdquirente retorna 0 y buscará RazonSocial en el API con el RUC.
         db = BD()
         sql = """
-            SELECT idAdquirente, RazonSocial, NombreComercial, LineaAsignada, TEA, TEM, Estado
+            SELECT idAdquirente, RazonSocial, NombreComercial, LineaAsignada, TEA, TEM, FactorAdelanto, Estado
             FROM empresas e inner join adquirentes a on e.RUC = a.RUC 
             WHERE e.RUC = %s
         """
@@ -82,15 +103,17 @@ class Adquirente:
             return recordset[0]
 
         razon_social = consultar_ruc_api(RUC)
-        TEA = _calcular_tea_adquirente_por_ranking_top(RUC)
+        FactorAdelanto = _calcular_FactorAdelanto_adquirente_por_ranking_top(RUC)
+        TEA = _calcular_TEA_adquirente_por_ranking_top(RUC)
         TEM = calcular_TEM_desde_TEA(TEA)
-
+ 
         return {
             "idAdquirente": 0,
             "RazonSocial": razon_social,
             "NombreComercial": "",
-            "LineaAsignada": Decimal("0"),
+            "LineaAsignada": Decimal("0.00"),
             "TEA": TEA,
             "TEM": TEM,
+            "FactorAdelanto": FactorAdelanto,
             "Estado": ""
         }
